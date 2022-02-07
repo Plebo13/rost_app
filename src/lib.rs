f940617b1d2eaf94795d0d310cf3be1f7ede17ca
Lukas Brauckmann <lukas.brauckmann@gmail.com>
2022-02-07 18:10:25 +0100

    Extract method for price parsing.

--- src/lib.rs
+++ src/lib.rs
@@ -14,24 +14,32 @@ pub fn get_etf_price(isin: &str) -> Result<f32, &str> {
         let body = http_response.text();
         let body = match body {
             Ok(body) => body,
-            Err(_error) => return Err("Error during HTML parsing!"),
+            Err(_error) => return Err("Error parsing the HTML document!"),
         };
 
-        let fragment = Html::parse_fragment(&body);
-        let div_selector = Selector::parse("div.mono").unwrap();
-        let span_selector = Selector::parse("span").unwrap();
-        let parsed_price = fragment
-            .select(&div_selector)
-            .next()
-            .unwrap()
-            .select(&span_selector)
-            .next()
-            .unwrap()
-            .inner_html();
-        let price_string = parsed_price.replace(".", "").replace(",", ".");
-        let price: f32 = price_string.parse::<f32>().unwrap();
-        Ok(price)
+        let fragment: scraper::Html = Html::parse_fragment(&body);
+        let price = parse_asset_price(&fragment);
+        match price {
+            Some(price) => Ok(price),
+            None => Err("Error parsing the HTML document!"),
+        }
     } else {
         Err("Error response")
     }
+}
+
+fn parse_asset_price(fragment: &scraper::Html) -> Option<f32> {
+    let div_selector = Selector::parse("div.mono").unwrap();
+    let span_selector = Selector::parse("span").unwrap();
+    let parsed_price = fragment
+        .select(&div_selector)
+        .next()
+        .unwrap()
+        .select(&span_selector)
+        .next()
+        .unwrap()
+        .inner_html();
+    let price_string = parsed_price.replace(".", "").replace(",", ".");
+    let price: f32 = price_string.parse::<f32>().unwrap();
+    Some(price)
 }
