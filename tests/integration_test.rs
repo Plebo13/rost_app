fc865b139e9833775fd3dace10fb826c21c8a714
Lukas Brauckmann <lukas.brauckmann@gmail.com>
2022-02-11 18:56:11 +0000

    First working coin implementation.

--- tests/integration_test.rs
+++ tests/integration_test.rs
@@ -1,7 +1,14 @@
+use rost_app::Coin;
+
 #[test]
 fn test_invalid_isin() {
     assert_eq!(
-        rost_app::get_etf_price("invalid_isin").unwrap_err(),
+        rost_app::get_etf_price(String::from("invalid_isin")).unwrap_err(),
         "Error response!"
     );
+}
+
+#[test]
+fn test_get_coin_price() {
+    rost_app::get_coin_price(&Coin::Bitcoin).unwrap();
 }
