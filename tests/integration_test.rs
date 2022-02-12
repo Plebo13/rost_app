e64c1aa645cfe65c16a3fd98a52e2918efab107f
Lukas Brauckmann <lukas.brauckmann@gmail.com>
2022-02-12 17:56:58 +0100

    Add more tests.

--- tests/integration_test.rs
+++ tests/integration_test.rs
@@ -17,10 +17,26 @@ fn test_get_bitcoin() {
 }
 
 #[test]
+fn test_get_bitcoin_string() {
+    match rost_app::get_coin("bitcoin") {
+        Some(_coin) => assert!(matches!(&Coin::Bitcoin, _coin)),
+        None => panic!("No string returned!"),
+    }
+}
+
+#[test]
 fn test_get_ethereum() {
     match rost_app::get_coin_price(&Coin::Ethereum) {
         Ok(_price) => assert!(true),
         Err(_error) => panic!("Unexpected exception!"),
+    }
+}
+
+#[test]
+fn test_get_ethereum_string() {
+    match rost_app::get_coin("ethereum") {
+        Some(_coin) => assert!(matches!(&Coin::Ethereum, _coin)),
+        None => panic!("No string returned!"),
     }
 }
 
@@ -33,10 +49,26 @@ fn test_get_binance_coin() {
 }
 
 #[test]
+fn test_get_binance_coin_string() {
+    match rost_app::get_coin("binancecoin") {
+        Some(_coin) => assert!(matches!(&Coin::BinanceCoin, _coin)),
+        None => panic!("No string returned!"),
+    }
+}
+
+#[test]
 fn test_get_tether() {
     match rost_app::get_coin_price(&Coin::Tether) {
         Ok(_price) => assert!(true),
         Err(_error) => panic!("Unexpected exception!"),
+    }
+}
+
+#[test]
+fn test_get_tether_string() {
+    match rost_app::get_coin("tether") {
+        Some(_coin) => assert!(matches!(&Coin::Tether, _coin)),
+        None => panic!("No string returned!"),
     }
 }
 
@@ -49,10 +81,26 @@ fn test_get_solana() {
 }
 
 #[test]
+fn test_get_solana_string() {
+    match rost_app::get_coin("solana") {
+        Some(_coin) => assert!(matches!(&Coin::Solana, _coin)),
+        None => panic!("No string returned!"),
+    }
+}
+
+#[test]
 fn test_get_cardano() {
     match rost_app::get_coin_price(&Coin::Cardano) {
         Ok(_price) => assert!(true),
         Err(_error) => panic!("Unexpected exception!"),
+    }
+}
+
+#[test]
+fn test_get_cardano_string() {
+    match rost_app::get_coin("cardano") {
+        Some(_coin) => assert!(matches!(&Coin::Cardano, _coin)),
+        None => panic!("No string returned!"),
     }
 }
 
@@ -63,11 +111,28 @@ fn test_get_ripple() {
         Err(_error) => panic!("Unexpected exception!"),
     }
 }
+
+#[test]
+fn test_get_ripple_string() {
+    match rost_app::get_coin("ripple") {
+        Some(_coin) => assert!(matches!(&Coin::Ripple, _coin)),
+        None => panic!("No string returned!"),
+    }
+}
+
 #[test]
 fn test_get_usd_coin() {
     match rost_app::get_coin_price(&Coin::USDCoin) {
         Ok(_price) => assert!(true),
         Err(_error) => panic!("Unexpected exception!"),
+    }
+}
+
+#[test]
+fn test_get_usd_coin_string() {
+    match rost_app::get_coin("usd-coin") {
+        Some(_coin) => assert!(matches!(&Coin::USDCoin, _coin)),
+        None => panic!("No string returned!"),
     }
 }
 
@@ -80,9 +145,25 @@ fn test_get_polkadot() {
 }
 
 #[test]
+fn test_get_polkadot_string() {
+    match rost_app::get_coin("polkadot") {
+        Some(_coin) => assert!(matches!(&Coin::Polkadot, _coin)),
+        None => panic!("No string returned!"),
+    }
+}
+
+#[test]
 fn test_get_dogecoin() {
     match rost_app::get_coin_price(&Coin::Dogecoin) {
         Ok(_price) => assert!(true),
         Err(_error) => panic!("Unexpected exception!"),
+    }
+}
+
+#[test]
+fn test_get_dogecoin_string() {
+    match rost_app::get_coin("dogecoin") {
+        Some(_coin) => assert!(matches!(&Coin::Dogecoin, _coin)),
+        None => panic!("No string returned!"),
     }
 }
